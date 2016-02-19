9fb83eb3e87088c40ed9020c28e9f06087352fda
Arc0re <PaperboyCheero@yahoo.com>
2016-02-19 11:32:39 +0100

    verbose

--- ch_bg.sh
+++ ch_bg.sh
@@ -3,7 +3,9 @@ echo "Changing tab background color..."
 
 if [ "$1" == "-default" ]
 then
+    echo "Restoring default tab bg color..."
     echo -e "\033]6;1;bg;*;default\a"
+    echo "Done!"
 else
     echo -e "\033]6;1;bg;red;brightness;112\a"
     echo -e "\033]6;1;bg;green;brightness;46\a"
